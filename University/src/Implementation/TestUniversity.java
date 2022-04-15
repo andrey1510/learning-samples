ffbee729333e328acf600821131411afe8af9804
alkour <alkour@yandex.ru>
2022-04-15 21:16:48 +0300

    add Task for exception

--- University/src/Implementation/TestUniversity.java
+++ University/src/Implementation/TestUniversity.java
@@ -54,19 +54,12 @@ public class TestUniversity {
         System.out.println("== " + (studentJohn == anotherJohn));
         System.out.println("equals " + (studentJohn.equals(anotherJohn)));
 
-        // if( == )
-        // if( equals )
-
-        // подсчитать сколько всего студентов, сколько аспирантов, сколько учителей. +
-        //toString() переопределить у всех объектов - студентов, аспирантов, учителей. +
-
-//        System.out.println(String.format("This is student with id = %s and name = %s", staff1.getId(), staff1.getName()));
 
     }
 }
-// Create class automatic generator students, teachers and postgraduates
-// ArrayList<Staff> generateStaff(int quantity); 100, id ordered from 0 to 100
-// class StaffGenerator - static ArrayList<Staff> generateStaff(int quantity);
-// случайным образом создавать студентов, препов и аспирантов. из расчета 5 студентов, 1 преп, 2 аспиранта
-//        ArrayList<Staff> staffList = StaffGenerator.generateStaff(50);
-//        using instanseOf and typecast, подсчитаешь сколько на самом деле студентов, препов и аспирантов.
+
+/*
+* Задание. В университете должно быть не больше 30 человек. Создать пакет - exception.
+* Create custom Exception - UniversityLimitException. Use this UniversityLimitException in StaffGenerator.generateStaff()
+* to check if number less than 30
+* */
